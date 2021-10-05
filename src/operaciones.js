091c6c9df1e1637f37c9c24989fcefedbdf8524f
Diana Marlene <diana.sanchez9907@gmail.com>
2021-10-04 21:56:08 -0500

    webpack label

---
+++ src/operaciones.js
@@ -0,0 +1,14 @@
+export const suma = (num1,num2) =>{
+    return num1+num2;
+}
+
+const resta= (num1,num2) =>{
+    return num1-num2;
+}
+
+const multiplicación= (num1,num2) =>{
+    return num1*num2;
+}
+
+
+export const saludo = 'Hola soy Diana, saludos desde el modulo';
